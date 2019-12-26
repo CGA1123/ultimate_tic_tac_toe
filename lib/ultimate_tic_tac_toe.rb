f71f796929a5ef2eb2eb6d643efe8d4906d11ac9
Christian Gregg <c_arlt@hotmail.com>
2019-12-26 15:54:39 +0000

    Initial commit

---
+++ lib/ultimate_tic_tac_toe.rb
@@ -0,0 +1,6 @@
+require "ultimate_tic_tac_toe/version"
+
+module UltimateTicTacToe
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ultimate_tic_tac_toe/version.rb
@@ -0,0 +1,3 @@
+module UltimateTicTacToe
+  VERSION = "0.1.0"
+end
