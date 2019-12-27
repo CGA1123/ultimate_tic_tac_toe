b3e916347946957c125be4514999a0960434aaa2
Christian Gregg <c_arlt@hotmail.com>
2019-12-27 13:51:20 +0000

    Ultimate Tic-Tac-Toe

--- lib/ultimate_tic_tac_toe.rb
+++ lib/ultimate_tic_tac_toe.rb
@@ -4,6 +4,7 @@ module UltimateTicTacToe
   class Error < StandardError; end
 
   autoload :Board, "ultimate_tic_tac_toe/board"
+  autoload :UltimateBoard, "ultimate_tic_tac_toe/ultimate_board"
   autoload :Game, "ultimate_tic_tac_toe/game"
   autoload :GreedyPlayer, "ultimate_tic_tac_toe/greedy_player"
   autoload :RandomPlayer, "ultimate_tic_tac_toe/random_player"
---
+++ lib/ultimate_tic_tac_toe/ultimate_board.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal
+
+module UltimateTicTacToe
+  class UltimateBoard
+    SIZE = 9
+    WINS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+
+    def initialize(board = Array.new(SIZE) { Board.new }, last_move = nil)
+      @board = board
+      @last_move = last_move
+    end
+
+    def set(indices, value)
+      big, small = indices
+
+      new_board = @board[big].set(small, value)
+      @board[big] = new_board
+
+      self.class.new(@board, small)
+    end
+
+    def state
+      @state ||= calculate_state
+    end
+
+    def running?
+      state == :running
+    end
+
+    def win?
+      state == :win
+    end
+
+    def draw?
+      state == :draw
+    end
+
+    def winner
+      return @winner if defined?(@winner)
+
+      @winner =
+        WINS
+          .map { |i, j, k| [@board[i], @board[j], @board[k]] }
+          .select { |x| x.none?(&:running?) }
+          .map { |x| x.map(&:winner).uniq }
+          .find { |x| x.size == 1 && !x.first.nil? }
+          &.first
+    end
+
+    def available_moves
+      if @last_move.nil? || !@board[@last_move].running?
+        @board
+          .each_with_index
+          .select { |board, i| board.running? }
+          .map { |board, i| board.available_moves.map { |x| [i, x] } }
+          .flatten(1)
+      else
+        @board[@last_move]
+          .available_moves
+          .map { |x| [@last_move, x] }
+      end
+    end
+
+    private
+
+    def calculate_state
+      return :win if winner
+      return :draw if available_moves.empty?
+
+      :running
+    end
+  end
+end
