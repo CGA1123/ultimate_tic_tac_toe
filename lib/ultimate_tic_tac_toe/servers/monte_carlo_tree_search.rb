86592bf5aac4ad0741ee944f3730f153c66521fd
Christian Gregg <c_arlt@hotmail.com>
2020-01-03 11:44:58 +0000

    wip

---
+++ lib/ultimate_tic_tac_toe/servers/monte_carlo_tree_search.rb
@@ -0,0 +1,11 @@
+module UltimateTicTacToe
+  module Servers
+    # https://en.wikipedia.org/wiki/Monte_Carlo_tree_search
+    # https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe#Computer_implementations
+    class MonteCarloTreeSearch < Protos::PlayerService::Service
+      def play(game, _)
+        # TODO
+      end
+    end
+  end
+end
