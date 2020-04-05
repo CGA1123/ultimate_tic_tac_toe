ad10189f08be21dfffead5bcd5ca775c0736b7d0
Christian Gregg <c_arlt@hotmail.com>
2020-04-05 13:08:30 +0100

    Services::TicTacToe::Player, TicTacToe::GrpcPlayer

---
+++ lib/services.rb
@@ -0,0 +1,12 @@
+module Services
+  autoload :TicTacToe, "services/tic_tac_toe"
+
+  module_function
+
+  def start_service(handler, port)
+    s = GRPC::RpcServer.new
+    s.add_http2_port("0.0.0.0:#{port}", :this_port_is_insecure)
+    s.handle(handler)
+    s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
+  end
+end
---
+++ lib/tic_tac_toe/grpc_player.rb
@@ -0,0 +1,44 @@
+class TicTacToe
+  class GrpcPlayer
+    class << self
+      def encode_game(game)
+        ProtocolBuffers::TicTacToe::Game.new(
+          player: encode_player(game.player),
+          board: game.board.map { |cell| encode_player(cell) }
+        )
+      end
+
+      def decode_move(move)
+        move.index
+      end
+
+      private
+
+      def encode_player(player)
+        case player
+        when TicTacToe::EMPTY then :EMPTY
+        when TicTacToe::PLAYER_X then :X
+        when TicTacToe::PLAYER_O then :O
+        else
+          raise "Bad Player"
+        end
+      end
+    end
+
+    attr_reader :name
+
+    def initialize(address, name: "grpc_player:#{address}")
+      @name = name
+      @stub = ProtocolBuffers::TicTacToe::PlayerService::Stub.new(
+        address, :this_channel_is_insecure
+      )
+    end
+
+    def play(game)
+      encoded_game = self.class.encode_game(game)
+      move = @stub.play(encoded_game)
+
+      self.class.decode_move(move)
+    end
+  end
+end
